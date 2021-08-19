c1a8a4a6e92770bb641db8cc018ef38a54c77db3
RanggaGunarto <ranggagunarto@gmail.com>
2021-08-19 23:58:39 +0800

    TODO
    Branch of a mini project before the Starchucks-Review app. Didnt want to make a new repo.

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Buy Starchucks', key: '1' },
+    { text: 'Learn React Native', key: '2' },
+    { text: 'Get Worldbox', key: '3' }
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* Header */}
+
+      <View style={styles.content}>
+        {/* Form */}
+
+        <View style={styles.list}>
+          <FlatList 
+            data={ todos }
+            renderItem={ ({ item }) => (
+              <Text>{ item.text }</Text>
+            )}
+          />
+        </View>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textW: {
+    color: '#007a33',
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  content: {
+    padding: 40,
+  },
+
+  list: {
+    marginTop: 20,
+  }
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+# Starchucks Review
